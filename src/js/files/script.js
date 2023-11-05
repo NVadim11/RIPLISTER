1052e766cf7160d30b93cb27dde509feff5b1111
NVadim11 <githubxdev@gmail.com>
2023-11-05 21:00:35 +0200

    youtube player logic

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -35,50 +35,8 @@ if (menuCloseBtn) {
     });
 }
 
-// youtube player
-var tag = document.createElement('script');
 
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('youtube-player', {
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        let theOverlay = document.getElementById("youtube-overlay");
-        player.mute(); 
-        theOverlay.onclick = function() {
-            this.style.display = "none";
-            player.playVideo();
-            document.getElementById("demo").innerHTML =
-              "Hide overlay and play the video!";
-          };
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
 
 
 // for (let year = new Date().getFullYear() ; year <= 2023; year++) {
