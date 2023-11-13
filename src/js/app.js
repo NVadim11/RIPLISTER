66dc77488f02e24dda549e7a6ae720f4626a28c5
NVadim11 <githubxdev@gmail.com>
2023-11-13 19:24:17 +0200

    keep forwarding

--- src/js/app.js
+++ src/js/app.js
@@ -187,7 +187,7 @@ import "./files/sliders.js"
 // Модуль поекранної прокрутки
 // Документація: https://template.fls.guru/template-docs/modul-poekrannoj-prokrutki-stranicy-fullpage.html
 // Сніппет(HTML):
-// import './libs/fullpage.js'
+import './libs/fullpage.js'
 
 // Модуль паралаксу
 // Документація: https://template.fls.guru/template-docs/paralaks-pri-skroli.html
