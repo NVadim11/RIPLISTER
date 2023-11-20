9b10b1df5a728efba3cd56802e7be6a2d4fa5616
NVadim11 <githubxdev@gmail.com>
2023-11-20 22:09:17 +0200

    popup en / favicons / meta descr

--- dist/js/app.js
+++ dist/js/app.js
@@ -7052,7 +7052,16 @@
                         whiteTitleFirst: "Спадок пам'яті,",
                         whiteTitleSecond: "що об'єднує нас.",
                         promoVideoText: "Подивитись відео\tпро RIPlister",
-                        promoDisplayLng: "УКР"
+                        promoDisplayLng: "УКР",
+                        thanksPopupText: "Дякуємо вам за підписку!",
+                        promoVideoLabel: "ЗАКРИТИ",
+                        promoPopupTitle: "Спадок пам'яті, що об'єднує нас.",
+                        promoPopup__about1: "Ласкаво просимо до нашого проєкту! Ми - команда ентузіастів, які працюють над створенням унікального вебсайту, який надасть можливість легко знайти місце поховання людини, що вас цікавить, а також допомогти відшукати ваші коріння та родинні зв'язки.",
+                        promoPopup__about2: "Ми знаємо, як важливо мати зручну та надійну платформу, яка допоможе знайти поховання за ім'ям та прізвищем. Наша команда працює над створенням потужної бази даних, що охоплюватиме поховання з різних країн та регіонів, щоб забезпечити найактуальнішу інформацію.",
+                        promoPopup__about3: "Наш сайт також надасть можливість створити сторінку пам'яті для вашої рідної людини. На цій особистій сторінці ви зможете поділитися фотографіями, спогадами та іншими матеріалами, які нагадують вам про вашого близького. Це буде місце, де ви зможете зібрати пам'ять про вашого рідного та запросити інших людей долучитися до вшанування його пам'яті, залишивши слова шани, запалюючи віртуальну свічку чи зробивши підношення у вигляді віртуального букета квітів.",
+                        promoPopup__about4: "У нашому проєкті ми також пропонуємо різноманітні послуги, які допоможуть вам дбати про могилу вашого близького. Ви зможете замовити послугу з прибирання могили, яка дозволить підтримувати її в чистоті та порядку. Крім того, ми пропонуємо нанесення QR-коду на могилу, який дозволить людям перейти на сторінку пам'яті вашого рідного. Це створить можливість для кожного, хто відвідує могилу, дізнатися більше про людину, що похована там, і висловити свою шану та повагу.",
+                        promoPopup__about5: "Ми з великим запалом продовжуємо працювати над розробкою сайту, щоб забезпечити вам якісну та інтуїтивно зрозумілу платформу. Наша мета - зробити процес пошуку місць поховання, вшанування пам'яті та догляду за могилами простими та доступними для всіх.",
+                        promoPopup__about6: "Приєднуйтесь до нас, аби бути в курсі розвитку проєкту та мати можливість робити свій внесок у розширення світової бази поховань. Разом ми зможемо створити особливе місце, де пам'ять про наших рідних буде жити назавжди."
                     }
                 },
                 en: {
@@ -7066,7 +7075,16 @@
                         whiteTitleFirst: "Legacy of memory",
                         whiteTitleSecond: "that unites us.",
                         promoVideoText: "Watch a video about RIPlister",
-                        promoDisplayLng: "ENG"
+                        promoDisplayLng: "ENG",
+                        thanksPopupText: "Thank you for subscribing!",
+                        promoVideoLabel: "CLOSE",
+                        promoPopupTitle: "Legacy of memory that unites us.",
+                        promoPopup__about1: "Welcome to our project! We are a team of enthusiasts working on creating a unique website that will allow you to easily find the burial place of a person you're interested in and help you discover your roots and family connections.",
+                        promoPopup__about2: "We understand the importance of having a convenient and reliable platform that can help locate burials by name and surname. Our team is working on building a powerful database that will cover burials from different countries and regions to provide the most up-to-date information.",
+                        promoPopup__about3: "Our website will also provide the opportunity to create a memorial page for your loved ones. On this personal page, you'll be able to share photos, memories, and other materials that remind you of your dear ones. It will be a place where you can gather memories of your loved ones and invite others to join in honoring their memory by leaving words of tribute, lighting a virtual candle, or offering virtual flower arrangements.",
+                        promoPopup__about4: "In our project, we also offer various services to help you care for the grave of your loved one. You'll be able to request a grave maintenance service to keep it clean and tidy. Additionally, we provide the option to add a QR code to the grave, allowing people to visit the memorial page of your loved one. This creates an opportunity for anyone visiting the grave to learn more about the person buried there and express their respect and tribute.",
+                        promoPopup__about5: "We are passionate about continuing our work on developing the website to provide you with a high-quality and user-friendly platform. Our goal is to make the process of searching for burial places, honoring memories, and caring for graves simple and accessible to everyone.",
+                        promoPopup__about6: "Join us to stay updated on the project's development and have the opportunity to contribute to the expansion of the global burial database. Together, we can create a special place where the memory of our loved ones will live forever."
                     }
                 }
             }
@@ -7076,14 +7094,26 @@
         function updateContent() {
             document.querySelector(".promo__headerTitle").innerHTML = instance.t("promoTitle");
             document.querySelector(".promo__descr").innerHTML = instance.t("promoDescr");
-            document.querySelector(".promo__link").innerHTML = instance.t("promoLink");
+            document.querySelector(".promo__link a").innerHTML = instance.t("promoLink");
             document.querySelector(".promo__orangeText").innerHTML = instance.t("promoOrangeText");
             document.querySelector(".promo__input-btn").innerHTML = instance.t("promoInputBtn");
+            document.querySelector(".promoPopup__input-btn").innerHTML = instance.t("promoInputBtn");
             document.querySelector(".promo__socialsText").innerHTML = instance.t("socialsText");
+            document.querySelector(".promoPopup__socialsText h4").innerHTML = instance.t("socialsText");
             document.querySelector(".promo__whiteTitleFirst").innerHTML = instance.t("whiteTitleFirst");
             document.querySelector(".promo__whiteTitleSecond").innerHTML = instance.t("whiteTitleSecond");
             document.querySelector(".promo__video-text").innerHTML = instance.t("promoVideoText");
             document.querySelector(".headerAlt__langBtn span").innerHTML = instance.t("promoDisplayLng");
+            document.querySelector(".popupThanks__orangeText h4").innerHTML = instance.t("thanksPopupText");
+            document.querySelector(".promoPopup__orangeText h4").innerHTML = instance.t("promoOrangeText");
+            document.querySelector(".popupVideo__close label").innerHTML = instance.t("promoVideoLabel");
+            document.querySelector(".promoPopup__title").innerHTML = instance.t("promoPopupTitle");
+            document.querySelector(".promoPopup__about1").innerHTML = instance.t("promoPopup__about1");
+            document.querySelector(".promoPopup__about2").innerHTML = instance.t("promoPopup__about2");
+            document.querySelector(".promoPopup__about3").innerHTML = instance.t("promoPopup__about3");
+            document.querySelector(".promoPopup__about4").innerHTML = instance.t("promoPopup__about4");
+            document.querySelector(".promoPopup__about5").innerHTML = instance.t("promoPopup__about5");
+            document.querySelector(".promoPopup__about6").innerHTML = instance.t("promoPopup__about6");
         }
         function changeLng(lng) {
             instance.changeLanguage(lng);
