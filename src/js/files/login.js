1fcfa8d214ebefb06f116605d998adbf1677990a
Ekkaira <denysxchebotar@gmail.com>
2023-12-13 20:09:10 +0200

    login logic, ready to test

--- src/js/files/login.js
+++ src/js/files/login.js
@@ -2,78 +2,86 @@ const email = document.getElementById("popupLoginForm__email");
 const password = document.getElementById("popupLoginForm__password"); 
 const form = document.getElementById("popupLoginForm");
 const successMsg = document.querySelector(".popupLoginForm__successMsg");
+let emailError = document.getElementById("popupLoginForm__emailError");
+let passwordError = document.getElementById("popupLoginForm__passwordError");
+let userError = document.getElementById("popupLoginForm__invalidUser");
 
+import usersDB from "../JSON/accounts.json"
+console.log(usersDB.users);
 
-const ajaxhttp = new XMLHttpRequest();
-const usersDB = "../JSON/accounts.json";
+// const users = [
+		// {
+		// 	"id": 1,
+		// 	"first_name": "John",
+		// 	"last_name": "Doe",
+		// 	"email": "johndoe@gmail.com",
+		// 	"username": "john20",
+		// 	"password": "Johntest123"
+		// },
+		// {
+		// 	"id": 2,
+		// 	"first_name": "Julia",
+		// 	"last_name": "Chan",
+		// 	"email": "juliachan@gmail.com",
+		// 	"username": "julia19",
+		// 	"password": "Juliatest123"
+		// }
+// 	]
 
-ajaxhttp.open('GET', usersDB, true);
-ajaxhttp.setRequestHeader("content-type","application/json");
+function login() {
+    let emailInput = email.value;
+    let passwordInput = password.value;
 
-ajaxhttp.onreadystatechange = function () {
-	if(ajaxhttp.readyState == 4 && ajaxhttp.status == 200) {
-		const jsonData = JSON.parse(ajaxhttp.responseText);
+    let userFound = usersDB.users.find(user => user.email === emailInput && user.password === passwordInput)
 
-		document.addEventListener("submit", formValidate);
-		
-		let i;
-		for(i = 0; i < jsonData.users.length; i++) {
-            output.innerHTML = jsonData.users[i].first_name + ' ' + jsonData.users[i].last_name;
-            // output2.innerHTML = jcontent.users[i].first_name + ' ' + jcontent.users[i].last_name;
-            console.log(jsonData.users[i].first_name + ' ' + jsonData.users[i].last_name);
-        }
-
-		console.log(jsonData);
-        console.log(jsonData.users[0].first_name);
-	}
+    if (userFound) {
+        email.parentElement.classList.remove("error");
+        password.parentElement.classList.remove("error")
+        console.log("Login Success");
+        console.log(userFound)
+    } else {
+        generateError("invalidUser", "Email or password is incorrect");
+        email.parentElement.classList.add("error");
+        password.parentElement.classList.add("error");
+    }
 }
 
-ajaxhttp.send(null);
-// output.innerHTML = "nothing";
-
-
 // Function to validate the email
-const validateEmail = (inputEmail)=> inputEmail.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+const validateEmail = (inputEmail)=> inputEmail.value.match(/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/);
 
-// Function to validate password
-const validatePassword = (inputPassword) => inputPassword.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+// Validation example Password123, at least one Upper case and one number.
+const validatePassword = (inputPassword) => inputPassword.value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/);
+
+// сделать проверку по языку для сообщений 
+function formValidate (inputEmail, inputPassword) {
+    if(!validateEmail(inputEmail)){
+        emailError = "please enter a valid email address";
+        generateError("email", emailError);
+        return;
+    }
+    if(!validatePassword(inputPassword)){
+        console.log("invalid password")
+        passwordError = "please enter correct password";
+        generateError("password", passwordError);
+        return;
+    }
+}
 
 // Function used to display errors
-const generateError = (errorName, errorMsg) =>{
-    const emailError = document.getElementById("popupLoginForm__emailError");
-    const passwordError = document.getElementById("popupLoginForm__passwordError");
+function generateError(errorName, errorMsg) {
     if(errorName == "email"){
         emailError.innerText = errorMsg;    
     }else if(errorName == "password"){
         passwordError.innerText = errorMsg;
+    } else if(errorName == "invalidUser") {
+        userError.innerHTML = errorMsg;
     }
 }
-
-// сделать проверку по языку для сообщений ошибки
-const formValidate = (inputEmail, inputPassword) =>{
-    if(!validateEmail(inputEmail)){
-        emailError = "please enter a valid email address";
-        generateError("email",emailError);
-        return;
-    }
-    if(!validatePassword(inputPassword)){
-        passwordError = "please enter correct password";
-        generateError(generateError("password",passwordError));
-        return;
-    }
-}
-
-//triggers when user submits the form
-form.addEventListener("submit",(e) => {
-    e.preventDefault();
-    formValidate(email, password);
-});
 
 // Focusout event listener. Triggers when the user clicks anywhere else besides the input
 email.addEventListener("focusout", (e)=>{
     if(!validateEmail(email)){
-        email.style.borderColor = "red";
-        generateError("email", "Please enter a valid email");
+        generateError("email", "This field is required");
         email.parentElement.classList.add("error");
     }
 });
@@ -81,8 +89,33 @@ email.addEventListener("focusout", (e)=>{
 // Focusout event listener triggers when the user clicks anywhere else besides the input
 password.addEventListener("focusout", (e)=>{
     if(!validatePassword(password)){
-        password.style.borderColor = "red";
-        generateError("password", "Please enter a valid password");
+        generateError("password", "This field is required");
         password.parentElement.classList.add("error");
     }
+});
+
+// FocusIn event listener. Triggers when the user clicks anywhere else besides the input
+email.addEventListener("focusin", (e)=>{
+    if(!validateEmail(email)){
+        emailError.innerText = "";    
+        userError.innerText = "";
+        email.parentElement.classList.remove("error");
+    }
+});
+
+// FocusIn event listener triggers when the user clicks anywhere else besides the input
+password.addEventListener("focusin", (e)=>{
+    if(!validatePassword(password)){      
+        passwordError.innerText = "";
+        userError.innerText = "";  
+        password.parentElement.classList.remove("error");
+    }
+});
+
+//triggers when user submits the form
+form.addEventListener("submit",(e) => {
+    e.preventDefault();
+    formValidate(email, password);
+    login();
+    e.target.reset();
 });
