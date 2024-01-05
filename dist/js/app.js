b5fafad87024710388d51be6ad0a0928617294f3
NVadim11 <githubxdev@gmail.com>
2024-01-05 15:58:04 +0200

    topBtn paggination and textBlock markup

--- dist/js/app.js
+++ dist/js/app.js
@@ -8889,6 +8889,35 @@
             console.log(formObject);
         }
         if (personalsEditForm) profile_validateForm("#personalsEdit__form", savePersonalData);
+        let up_to_top_requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+        document.addEventListener("DOMContentLoaded", (function() {
+            const goTopButton = document.querySelector('[data-action="gotop"]');
+            const windowViewPortHeight = window.innerHeight;
+            let isRequestingAnimationFrame = false;
+            if (!goTopButton) return;
+            goTopButton.addEventListener("click", (function() {
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth"
+                });
+            }));
+            window.addEventListener("scroll", (function() {
+                if (!isRequestingAnimationFrame) {
+                    up_to_top_requestAnimationFrame(filterGoTopButtonVisibility);
+                    isRequestingAnimationFrame = true;
+                }
+            }));
+            function filterGoTopButtonVisibility(timestamp) {
+                let windowPageYOffset = window.pageYOffset || document.documentElement.scrollTop;
+                if (windowPageYOffset > windowViewPortHeight) {
+                    goTopButton.classList.add("show");
+                    isRequestingAnimationFrame = false;
+                } else {
+                    goTopButton.classList.remove("show");
+                    up_to_top_requestAnimationFrame(filterGoTopButtonVisibility);
+                }
+            }
+        }));
         window["FLS"] = true;
         isWebp();
         menuInit();
